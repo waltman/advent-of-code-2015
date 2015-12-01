3d4ede88e10f2f3120f10a5a53ef49f328942704
Walt Mankowski <waltman@pobox.com>
2015-12-01 13:27:24 -0500

    code to solve both parts of the day 1 puzzle

---
+++ day01/not_quite_lisp.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.10.0;
+
+while (<>) {
+    my $floor = 0;
+    my $basement = 0;
+    chomp;
+    my @c = split //;
+    for my $i (0..$#c) {
+        my $c = $c[$i];
+        $floor++ if $c eq '(';
+        $floor-- if $c eq ')';
+        $basement = $i+1 if $floor < 0 && $basement == 0;
+    }
+    say "$floor $basement => $_";
+    $floor = 0;
+}
