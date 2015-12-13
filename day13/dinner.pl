7c6e2b6a3b70d25a3acc9d70e452df30ab77e791
Walt Mankowski <waltman-github@mawode.com>
2015-12-13 06:19:25 -0500

    code for day 13 part 1

---
+++ day13/dinner.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Algorithm::Combinatorics qw(permutations);
+use 5.22.0;
+
+my %names;
+my %happiness;
+
+# parse input
+while (<>) {
+    chomp;
+    s/\.//;
+    my @F = split / /;
+    my ($n1, $gl, $val, $n2) = @F[0, 2, 3, -1];
+    $names{$n1} = 1;
+    $happiness{"$n1,$n2"} = ($gl eq "gain") ? $val : -$val;
+}
+
+my @names = sort keys %names;
+my $high_happ;
+my $high_perm;
+my $iter = permutations(\@names);
+while (my $p = $iter->next) {
+    my $total = 0;
+    for my $i (0..$#$p) {
+        my $j = ($i + 1) % @names;
+        $total += $happiness{"$p->[$i],$p->[$j]"};
+        $total += $happiness{"$p->[$j],$p->[$i]"};
+    }
+
+    my $perm = join " ", @$p;
+    say "$perm = $total";
+
+    if (!defined $high_happ || $total > $high_happ) {
+        $high_happ = $total;
+        $high_perm = $perm;
+    }
+}
+
+print "\nhighest perm: $high_perm = $high_happ\n";
