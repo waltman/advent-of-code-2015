5e14090eb301e32e4247640c1c53a62ee5ee9051
Walt Mankowski <waltman-github@mawode.com>
2015-12-24 14:04:56 -0500

    only add partitions containing the smallest group

--- day24/qe.pl
+++ day24/qe.pl
@@ -14,23 +14,44 @@ while (<>) {
 my $target = sum(@packages) / 3;
 my %seen;
 my @possible;
+say "target = $target";
 
 my $iter = partitions(\@packages, 2);
+my $n = 0;
+$| = 1;
+my $smallest = @packages;
 while (my $p = $iter->next) {
+    $n++;
+    if ($n % 100_000 == 0) {
+        printf "iteration %d, found %d possibilites, smallest = %d\n", $n, scalar @possible, $smallest;
+    }
+
     if (sum(@{$p->[0]}) == $target && !$seen{"@{$p->[0]}"}) {
         $seen{"@{$p->[0]}"} = 1;
+#        say "checking @{$p->[0]}";
         my $iter2 = partitions($p->[1], 2);
+        my $size1 = @{$p->[0]};
         while (my $p2 = $iter2->next) {
             if (sum(@{$p2->[0]}) == $target && !$seen{"@{$p2->[0]}"}) {
                 $seen{"@{$p2->[0]}"} = 1;
                 $seen{"@{$p2->[1]}"} = 1;
+                my $size2 = @{$p2->[0]};
+                my $size3 = @{$p2->[1]};
+                my $small = min($size1, $size2, $size3);
+                next if $small > $smallest;
+                if ($small < $smallest) {
+                    @possible = ();
+                    $smallest = $small;
+                }
                 push @possible, [$p->[0], $p2->[0], $p2->[1]];
+#                say "(@{$p->[0]}) (@{$p2->[0]}) (@{$p2->[1]})";
             }
         }
     }
 }
 
 # for my $p (sort smallest_group @possible) {
+say "\nPossibilities";
 for my $p (@possible) {
     say "(@{$p->[0]}) (@{$p->[1]}) (@{$p->[2]})"
 }
