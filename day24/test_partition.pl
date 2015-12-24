5f7aa821801dd3870c0194934fac1469dace5244
Walt Mankowski <waltman-github@mawode.com>
2015-12-24 13:33:46 -0500

    sample code to test partitioning

--- day24/test_partition.pl
+++ day24/test_partition.pl
@@ -2,21 +2,25 @@
 use strict;
 use warnings;
 use Algorithm::Combinatorics qw(partitions);
+use List::Util qw(sum);
 use 5.18.0;
 
-my @packages = (1..5, 7..11);
+#my @packages = (1..5, 7..11);
 #say "@packages";
 
-# my @packages;
-# while (<>) {
-#     chomp;
-#     push @packages, $_;
-# }
-
-# say "@packages";
-# say scalar @packages;
-
-my $iter = partitions(\@packages, 5);
-while (my $p = $iter->next) {
-    say "(@{$p->[0]}) (@{$p->[1]}) (@{$p->[2]})";
+my @packages;
+while (<>) {
+    chomp;
+    push @packages, $_;
 }
+
+
+
+say "@packages";
+say scalar @packages;
+say sum @packages;
+
+# my $iter = partitions(\@packages, 5);
+# while (my $p = $iter->next) {
+#     say "(@{$p->[0]}) (@{$p->[1]}) (@{$p->[2]})";
+# }
