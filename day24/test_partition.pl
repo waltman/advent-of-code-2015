e61dbe47238798ad787d15634fc2542e1227ea31
Walt Mankowski <waltman-github@mawode.com>
2015-12-24 10:20:41 -0500

    test code to partitiion the set for day 24 problem

---
+++ day24/test_partition.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Algorithm::Combinatorics qw(partitions);
+use 5.18.0;
+
+my @packages = (1..5, 7..11);
+#say "@packages";
+
+# my @packages;
+# while (<>) {
+#     chomp;
+#     push @packages, $_;
+# }
+
+# say "@packages";
+# say scalar @packages;
+
+my $iter = partitions(\@packages, 5);
+while (my $p = $iter->next) {
+    say "(@{$p->[0]}) (@{$p->[1]}) (@{$p->[2]})";
+}
