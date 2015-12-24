acdf7fe97491f72edb340aa65c374c4e5d00b528
Walt Mankowski <waltman-github@mawode.com>
2015-12-24 14:18:07 -0500

    only keep the partition with the best qe

--- day24/qe.pl
+++ day24/qe.pl
@@ -20,10 +20,11 @@ my $iter = partitions(\@packages, 2);
 my $n = 0;
 $| = 1;
 my $smallest = @packages;
+my $smallest_qe = 1e300;
 while (my $p = $iter->next) {
     $n++;
     if ($n % 100_000 == 0) {
-        printf "iteration %d, found %d possibilites, smallest = %d\n", $n, scalar @possible, $smallest;
+        printf "iteration %d, found %d possibilites, smallest = %d, smallest_qe = %d\n", $n, scalar @possible, $smallest, $smallest_qe;
     }
 
     if (sum(@{$p->[0]}) == $target && !$seen{"@{$p->[0]}"}) {
@@ -31,17 +32,28 @@ while (my $p = $iter->next) {
 #        say "checking @{$p->[0]}";
         my $iter2 = partitions($p->[1], 2);
         my $size1 = @{$p->[0]};
+        my $qe1 = prod($p->[0]);
         while (my $p2 = $iter2->next) {
             if (sum(@{$p2->[0]}) == $target && !$seen{"@{$p2->[0]}"}) {
                 $seen{"@{$p2->[0]}"} = 1;
                 $seen{"@{$p2->[1]}"} = 1;
                 my $size2 = @{$p2->[0]};
                 my $size3 = @{$p2->[1]};
+                my $qe2 = prod($p2->[0]);
+                my $qe3 = prod($p2->[1]);
                 my $small = min($size1, $size2, $size3);
+                my $low_qe = min($qe1, $qe2, $qe3);
                 next if $small > $smallest;
                 if ($small < $smallest) {
                     @possible = ();
                     $smallest = $small;
+                    $smallest_qe = $low_qe;
+                } elsif ($low_qe <= $smallest_qe) {
+                    @possible = ();
+                    $smallest = $small;
+                    $smallest_qe = $low_qe;
+                } else {
+                    next;
                 }
                 push @possible, [$p->[0], $p2->[0], $p2->[1]];
 #                say "(@{$p->[0]}) (@{$p2->[0]}) (@{$p2->[1]})";
@@ -55,6 +67,7 @@ say "\nPossibilities";
 for my $p (@possible) {
     say "(@{$p->[0]}) (@{$p->[1]}) (@{$p->[2]})"
 }
+say "low qe = $smallest_qe";
 
 sub smallest_group {
     my ($x, $y) = @_;
@@ -65,3 +78,11 @@ sub smallest_group {
     return $x1 <=> $y1;
 }
 
+sub prod {
+    my $ar = shift;
+
+    my $prod = 1;
+    $prod *= $_ for @$ar;
+
+    return $prod;
+}
